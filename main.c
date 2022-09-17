defbea68fa796fcdfdcf32bc9701992ebef3e5e5
hum <65712791+hum@users.noreply.github.com>
2022-09-17 12:30:28 +0100

    basic alloc

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
-#include <sys/types.h>
+#include <assert.h>
 #include <stdio.h>
+#include <sys/types.h>
 
 #define HEAP_MAXSIZE 65535
 
@@ -7,11 +8,23 @@
 char heap[HEAP_MAXSIZE] = {0};
 size_t heap_size = 0;
 
-void heap_malloc(size_t size) { }
+void *heap_malloc(size_t size) { 
+  assert(heap_size + size <= HEAP_MAXSIZE);
 
-void heap_free(void *ptr) { }
+  void* result = heap + heap_size;
+  heap_size += size;
+  return result;
+}
+
+void heap_free(void *ptr);
+void *realloc(void *ptr, size_t size);
 
 int main() {
-  printf("hello, world\n");
+  char *mem_root = heap_malloc(20);
+  for (int i = 0; i < 20; i++) {
+    mem_root[i] = 'A' + i;
+    printf("%c", mem_root[i]);
+  }
+  printf("\n");
   return 0;
 }
